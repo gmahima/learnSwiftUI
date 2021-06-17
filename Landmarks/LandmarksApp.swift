fc46f7911e32e0310dc5f5cbe455b333a54c0c03
gmahima <mahimagangavarapu@gmail.com>
2021-06-17 11:00:05 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by G H Mahimaanvita on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
